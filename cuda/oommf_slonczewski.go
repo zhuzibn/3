885977b90c1909476d0e3d803737e0471e25f277
Xuanyao Fong <xuanyao.fong@gmail.com>
2016-10-24 20:17:29 +0800

    Merge all kelvin_* under nrl branch

---
+++ cuda/oommf_slonczewski.go
@@ -0,0 +1,30 @@
+package cuda
+
+import (
+	"github.com/mumax/3/data"
+	"unsafe"
+)
+
+// Add Slonczewski ST torque to torque (Tesla).
+// see slonczewski.cu
+func AddOommfSlonczewskiTorque(torque, m *data.Slice, Msat, J, fixedP, alpha, pfix, pfree, λfix, λfree, ε_prime MSlice, mesh *data.Mesh) {
+	N := torque.Len()
+	cfg := make1DConf(N)
+	thickness := float32(mesh.WorldSize()[Z])
+
+	k_addoommfslonczewskitorque_async(torque.DevPtr(X), torque.DevPtr(Y), torque.DevPtr(Z),
+		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
+		Msat.DevPtr(0), Msat.Mul(0),
+		J.DevPtr(Z), J.Mul(Z),
+		fixedP.DevPtr(X), fixedP.Mul(X),
+		fixedP.DevPtr(Y), fixedP.Mul(Y),
+		fixedP.DevPtr(Z), fixedP.Mul(Z),
+		alpha.DevPtr(0), alpha.Mul(0),
+		pfix.DevPtr(0), pfix.Mul(0),
+		pfree.DevPtr(0), pfree.Mul(0),
+		λfix.DevPtr(0), λfix.Mul(0),
+		λfree.DevPtr(0), λfree.Mul(0),
+		ε_prime.DevPtr(0), ε_prime.Mul(0),
+		unsafe.Pointer(uintptr(0)), thickness,
+		N, cfg)
+}
